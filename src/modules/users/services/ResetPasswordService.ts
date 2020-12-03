d11f013e053e00ef14c916923371352d9e39df79
João Mangueira <jamjs7@gmail.com>
2020-12-03 19:55:46 -0300

    Modulo 4 - Pefil de usuario aula 2

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,45 @@
+import { injectable, inject } from 'tsyringe';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+import UsersRepository from '@modules/users/infra/http/repositories/UsersRepository';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import User from "@modules/users/infra/typeorm/entities/User";
+import AppError from "@shared/errors/AppError";
+
+
+interface IRequest {
+    name: string;
+    email: string;
+    password: string;
+}
+
+@injectable()
+class CreateUserService {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+
+        @inject('HashProvider')
+        private hashProvider: IHashProvider,
+    ) {}
+
+    public async execute({ name, email, password } : IRequest): Promise<User> {
+        const checkUserExists = await this.usersRepository.findByEmail(email);
+
+        if(checkUserExists) {
+            throw new AppError('Email address already used.');
+        }
+
+        const hashedPassword = await this.hashProvider.generateHash(password);
+
+        const user = await this.usersRepository.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        return user;
+    }
+}
+
+export default CreateUserService;
