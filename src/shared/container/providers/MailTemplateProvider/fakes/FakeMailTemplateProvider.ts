21f86b9ecebb55865cde2e468c2bb919b895f88c
João Mangueira <jamjs7@gmail.com>
2020-12-04 12:57:46 -0300

    Modulo 4 - Perfil de usuario aula 7

---
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -0,0 +1,9 @@
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import IParseMailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
+
+export default class FakeMailTemplateProvider implements IMailTemplateProvider {
+
+    public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
+        return template;
+    }
+}
