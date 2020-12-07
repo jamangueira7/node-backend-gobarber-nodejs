7eb1f0f24ca016a45cbd0b5dca22c13da2f7c8fa
João Mangueira <jamjs7@gmail.com>
2020-12-07 19:52:43 -0300

    Modulo 4 - Personalizando Cache e segurança aula 03

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -3,8 +3,9 @@ import IHashProvider from '@modules/users/providers/HashProvider/models/IHashPro
 import UsersRepository from '@modules/users/infra/http/repositories/UsersRepository';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import User from "@modules/users/infra/typeorm/entities/User";
-import AppError from "@shared/errors/AppError";
+import User from '@modules/users/infra/typeorm/entities/User';
+import AppError from '@shared/errors/AppError';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 
 interface IRequest {
@@ -21,6 +22,9 @@ class CreateUserService {
 
         @inject('HashProvider')
         private hashProvider: IHashProvider,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {}
 
     public async execute({ name, email, password } : IRequest): Promise<User> {
@@ -37,6 +41,8 @@ class CreateUserService {
             email,
             password: hashedPassword,
         });
+
+        await this.cacheProvider.invalidatePrefix(`providers-list:`);
 
         return user;
     }
